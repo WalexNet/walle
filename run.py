674abe878e1f463b3ab254b38d209f45cd313302
WalexNet <walter@walex.net>
2026-02-19 12:40:56 +0000

    Cambios a ser confirmados:   modificados:     run.py
    cambiamos el puerto de inicio de la app y empezamos a usar el gunicorn

--- run.py
+++ run.py
@@ -895,5 +895,5 @@ def auditoria_fw(archivo_conf, ver):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0')#, port=8080)
 
